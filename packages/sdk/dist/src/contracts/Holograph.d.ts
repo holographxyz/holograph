41cc7beb8faf7b63cb7fa59030573828bfaf2159
Alexander <alexanderattar@gmail.com>
2023-07-19 00:47:52 -0400

    Make sure dist folders are ignored

--- packages/sdk/dist/src/contracts/Holograph.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class Holograph {
-}
-//# sourceMappingURL=Holograph.d.ts.map
--- packages/sdk/dist/src/contracts/Holograph.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Holograph = void 0;
-class Holograph {
-}
-exports.Holograph = Holograph;
-//# sourceMappingURL=Holograph.js.map
