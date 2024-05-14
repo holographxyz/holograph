1856ac30428870064e7c63d8e52722156e2385bc
Felipe Rodrigues <felipe.augusto.rodrigues1@gmail.com>
2024-05-14 16:15:55 -0300

    chore: Add exports

---
+++ packages/sdk/src/index.ts
@@ -0,0 +1,22 @@
+import {BridgeCollection} from './assets/bridge-collection'
+import {BridgeNFT} from './assets/bridge-nft'
+import {HolographLegacyCollection} from './assets/collection-legacy'
+import {HolographMoeERC721DropV1, HolographMoeERC721DropV2} from './assets/collection-moe'
+import {NFT} from './assets/nft'
+import {MoeNFT} from './assets/nft-moe'
+import {HolographAccountFactory, HolographWallet} from './services'
+import {DeploymentConfig, ERC721Config, HolographConfig, Signature, SignDeploy} from './utils/types'
+
+export {
+  HolographLegacyCollection,
+  HolographMoeERC721DropV1,
+  HolographMoeERC721DropV2,
+  NFT,
+  MoeNFT,
+  BridgeNFT,
+  BridgeCollection,
+  HolographAccountFactory,
+  HolographWallet,
+}
+
+export type {DeploymentConfig, ERC721Config, HolographConfig, Signature, SignDeploy}
