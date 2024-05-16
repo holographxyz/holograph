69e7c3430e2249b2c99a58d65fe4eee3be688e61
Felipe Rodrigues <felipe.augusto.rodrigues1@gmail.com>
2024-05-16 00:38:55 -0300

    chore: Hide pino logger imports

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,5 +1,5 @@
-import {BridgeCollection} from './assets/bridge-collection'
-import {BridgeNFT} from './assets/bridge-nft'
+// import {BridgeCollection} from './assets/bridge-collection'
+// import {BridgeNFT} from './assets/bridge-nft'
 import {HolographLegacyCollection} from './assets/collection-legacy'
 import {HolographMoeERC721DropV1, HolographMoeERC721DropV2} from './assets/collection-moe'
 import {NFT} from './assets/nft'
@@ -7,14 +7,16 @@ import {MoeNFT} from './assets/nft-moe'
 import {HolographAccountFactory, HolographWallet} from './services'
 import {DeploymentConfig, ERC721Config, HolographConfig, Signature, SignDeploy} from './utils/types'
 
+// TODO: Uncomment Bridge class imports after resolving the dependency issue for the FE integration
+
 export {
   HolographLegacyCollection,
   HolographMoeERC721DropV1,
   HolographMoeERC721DropV2,
   NFT,
   MoeNFT,
-  BridgeNFT,
-  BridgeCollection,
+  // BridgeNFT,
+  // BridgeCollection,
   HolographAccountFactory,
   HolographWallet,
 }
