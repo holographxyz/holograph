3233c21211836d818f572ee6f881ddd1ba8179c6
Felipe Rodrigues <felipe.augusto.rodrigues1@gmail.com>
2024-05-16 19:10:09 -0300

    refactor: Update logger service

--- packages/sdk/src/services/logger.service.ts
+++ packages/sdk/src/services/logger.service.ts
@@ -1,10 +1,21 @@
-import pino from 'pino'
+// import pino from 'pino'
+// import pretty from 'pino-pretty'
+import {Console} from 'console'
 import {v4 as uuidv4} from 'uuid'
 
-import {getLoggerConfigs} from '../config/logger.config'
+// import {getLoggerConfigs} from '../config/logger.config'
+// import {baseClassSimulacrum} from '../utils/transformers'
 import {HolographError} from '../errors'
-import {baseClassSimulacrum} from '../utils/transformers'
-import {HolographLoggerContext, PinoMethods} from '../utils/types'
+import {
+  HolographLoggerContext,
+  // PinoMethods
+} from '../utils/types'
+
+type LogLevelType = 'debug' | 'error' | 'info' | 'log' | 'trace' | 'warn'
+
+const Logger = (logLevel: LogLevelType, ...args) => {
+  console[logLevel](...args)
+}
 
 export function simpleFnLogger() {
   return function (target: Object, key: string, descriptor: PropertyDescriptor) {
@@ -22,59 +33,38 @@ export function simpleFnLogger() {
   }
 }
 
-export class HolographLogger extends baseClassSimulacrum<PinoMethods>() {
-  protected static config = getLoggerConfigs()
+export class HolographLogger extends Console {
+  private constructor(protected context: HolographLoggerContext) {
+    super(process.stdout, process.stderr)
 
-  private constructor(private pinoLogger: pino.Logger, protected context: HolographLoggerContext) {
-    super()
     return new Proxy(this, {
-      get(target, prop) {
+      get(target, prop: LogLevelType) {
         const exists = target[prop as keyof typeof target]
 
         if (exists !== undefined) return exists
 
-        const underlyingAtt = target.pinoLogger[prop as keyof typeof target.pinoLogger]
-
-        if (typeof underlyingAtt === 'function') {
-          return underlyingAtt.bind(target.pinoLogger)
-        }
-
-        return target.pinoLogger[prop as keyof typeof target.pinoLogger]
+        return Logger.bind(null, prop)
       },
     })
   }
 
   logHolographError(error: HolographError) {
-    this.pinoLogger.debug(error.stack) // log stack if in debug mode
     if (error.message) {
-      this.pinoLogger.info(`${error.code}: ${error.message}`) // nice error output
+      this.info(`${error.code}: ${error.message}`) // nice error output
     } else {
       // log the internal error that we have not captured yet
-      this.pinoLogger.error(error.cause)
+      this.error(error.cause)
     }
   }
 
-  getContext() {
-    // Notice: make sure this.context is equal to this.pinoLogger.bindings()
-    return this.context
-  }
-
   static createLogger(input: HolographLoggerContext) {
-    const pinoLogger = pino({
-      ...HolographLogger.config,
-      browser: {
-        serialize: true,
-        asObject: true,
-      },
-    }).child(input)
-
-    return new HolographLogger(pinoLogger, input)
+    return new HolographLogger(input)
   }
 
   static maybeAddTraceId(logger: HolographLogger) {
     if (!logger.context.traceId) {
       const traceId = uuidv4()
-      return HolographLogger.createLogger({...logger.getContext(), traceId})
+      return HolographLogger.createLogger({...logger.context, traceId})
     }
     return logger
   }
@@ -92,7 +82,82 @@ export class HolographLogger extends baseClassSimulacrum<PinoMethods>() {
       newContext.traceId = uuidv4()
     }
 
-    const thisLoggerChild = this.pinoLogger.child(newContext)
-    return new HolographLogger(thisLoggerChild, newContext)
+    return new HolographLogger(newContext)
   }
 }
+
+// TODO: Remove the above version of HolographLogger and uncomment lines below resolving the dependency issue for the FE integration
+
+// export class HolographLogger extends baseClassSimulacrum<PinoMethods>() {
+//   protected static config = getLoggerConfigs()
+
+//   private constructor(private pinoLogger: pino.Logger, protected context: HolographLoggerContext) {
+//     super()
+//     return new Proxy(this, {
+//       get(target, prop) {
+//         const exists = target[prop as keyof typeof target]
+
+//         if (exists !== undefined) return exists
+
+//         const underlyingAtt = target.pinoLogger[prop as keyof typeof target.pinoLogger]
+
+//         if (typeof underlyingAtt === 'function') {
+//           return underlyingAtt.bind(target.pinoLogger)
+//         }
+
+//         return target.pinoLogger[prop as keyof typeof target.pinoLogger]
+//       },
+//     })
+//   }
+
+//   logHolographError(error: HolographError) {
+//     this.pinoLogger.debug(error.stack) // log stack if in debug mode
+//     if (error.message) {
+//       this.pinoLogger.info(`${error.code}: ${error.message}`) // nice error output
+//     } else {
+//       // log the internal error that we have not captured yet
+//       this.pinoLogger.error(error.cause)
+//     }
+//   }
+
+//   getContext() {
+//     // Notice: make sure this.context is equal to this.pinoLogger.bindings()
+//     return this.context
+//   }
+
+//   static createLogger(input: HolographLoggerContext) {
+//      const pinoLogger = pino(
+//       HolographLogger.config,
+//       pretty({
+//        colorize: true,
+//       }),
+//      ).child(input)
+
+//     return new HolographLogger(pinoLogger, input)
+//   }
+
+//   static maybeAddTraceId(logger: HolographLogger) {
+//     if (!logger.context.traceId) {
+//       const traceId = uuidv4()
+//       return HolographLogger.createLogger({...logger.getContext(), traceId})
+//     }
+//     return logger
+//   }
+
+//   addContext(context: HolographLoggerContext) {
+//     const newContext = {...this.context}
+
+//     for (const prop in context) {
+//       if (!this.context[prop]) {
+//         newContext[prop] = context[prop]
+//       }
+//     }
+
+//     if (!newContext.traceId && newContext.functionName !== undefined) {
+//       newContext.traceId = uuidv4()
+//     }
+
+//     const thisLoggerChild = this.pinoLogger.child(newContext)
+//     return new HolographLogger(thisLoggerChild, newContext)
+//   }
+// }
--- packages/sdk/src/services/wallet.service.ts
+++ packages/sdk/src/services/wallet.service.ts
@@ -23,12 +23,10 @@ import {
   UnavailableNetworkError,
   WalletNotFoundError,
 } from '../errors'
-import {Config} from './config.service'
 import {getEnvRpcConfig, isFrontEnd} from '../utils/helpers'
 import {holographToViemChain} from '../utils/transformers'
 import {HolographAccount, HolographWalletArgs} from '../utils/types'
-
-// TODO: Uncomment HolographLogger lines after resolving the dependency issue for the FE integration
+import {Config, HolographLogger} from '.'
 
 /**
  * HolographAccountFactory
@@ -76,13 +74,13 @@ export class HolographAccountFactory {
  *
  */
 export class HolographWallet {
-  // private readonly _logger: HolographLogger
+  private readonly _logger: HolographLogger
   private _account: HolographAccount
   private _networks?: Network[]
   private _multiChainWalletClient: Record<number, WalletClient & PublicActions> = {}
 
   constructor({account, networks, chainsRpc}: HolographWalletArgs) {
-    // this._logger = HolographLogger.createLogger({serviceName: HolographWallet.name})
+    this._logger = HolographLogger.createLogger({serviceName: HolographWallet.name})
     this._account = account
     let chainsRpc_ = chainsRpc
 
@@ -119,8 +117,8 @@ export class HolographWallet {
   }
 
   onChain(chainId: number): WalletClient & PublicActions {
-    // const logger = this._logger.addContext({functionName: this.onChain.name})
-    // logger.info(`wallet client accessing chainId = ${chainId}`)
+    const logger = this._logger.addContext({functionName: this.onChain.name})
+    logger.info(`wallet client accessing chainId = ${chainId}`)
 
     const walletClient = this._multiChainWalletClient[chainId]
 
@@ -155,13 +153,13 @@ export class HolographWallet {
  *
  */
 export class HolographWalletManager {
-  // private readonly _logger: HolographLogger
+  private readonly _logger: HolographLogger
   private readonly _networks: Network[]
   private _wallets: {[accountName: string]: HolographWallet} = {}
   private _addressToAccountName: {[address: Address]: string} = {}
 
   constructor(private readonly protocolConfig: Config) {
-    // this._logger = HolographLogger.createLogger({serviceName: HolographWalletManager.name})
+    this._logger = HolographLogger.createLogger({serviceName: HolographWalletManager.name})
 
     if (this.protocolConfig.accounts?.default === undefined) {
       throw new MissingDefaultWalletError(HolographWalletManager.name)
@@ -204,11 +202,11 @@ export class HolographWalletManager {
   }
 
   addAccount(accountName: string, account: HolographAccount): HolographWallet {
-    // const logger = this._logger.addContext({functionName: this.addAccount.name})
+    const logger = this._logger.addContext({functionName: this.addAccount.name})
 
     let name = accountName
     if (this._wallets === undefined) {
-      // logger.warn(`Setting the "default" account to ${account}`)
+      logger.warn(`Setting the "default" account to ${account}`)
       name = 'default'
     }
 
@@ -223,14 +221,14 @@ export class HolographWalletManager {
   }
 
   updateAccount(account: HolographAccount, accountName: 'default'): HolographWallet {
-    // const logger = this._logger.addContext({functionName: this.updateAccount.name})
+    const logger = this._logger.addContext({functionName: this.updateAccount.name})
 
     const accountsNames = Object.keys(this._wallets)
     if (!accountsNames.includes(accountName)) {
       throw new WalletNotFoundError(accountName, this.updateAccount.name)
     }
 
-    // logger.warn(`WARN: "${accountName}" account is being overridden`)
+    logger.warn(`WARN: "${accountName}" account is being overridden`)
 
     // Update wallet clients
     this._wallets[accountName] = new HolographWallet({account, networks: this._networks})
